4ea82804531beb15e6580c44bbee400366816f54
Gilberto Farias de Sousa Filho <gilberto@dce.ufpb.br>
2012-06-15 16:48:49 -0300

    Eventos do ApresSwing adicionados.

---
+++ src/br/ufpb/iged/gui/event/ApresSwingEvent.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpb.iged.gui.event;
+
+import br.ufpb.iged.gui.ApresSwing;
+
+/**
+ *
+ * @author GILBERTO FARIAS
+ */
+public class ApresSwingEvent {
+    ApresSwing source = null;
+
+    public ApresSwing getSource() {
+        return source;
+    }
+
+    public void setSource(ApresSwing source) {
+        this.source = source;
+    }
+}
---
+++ src/br/ufpb/iged/gui/event/ApresSwingListener.java
@@ -0,0 +1,9 @@
+package br.ufpb.iged.gui.event;
+
+/**
+ *
+ * @author GILBERTO FARIAS
+ */
+public interface ApresSwingListener {
+    public abstract void receiveEvent(ApresSwingEvent ase);
+}
