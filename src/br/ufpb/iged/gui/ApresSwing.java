5b4945ed6779239d96c501c1e2cb52cf68b96859
ErickCosta <erick.costa@@dce.ufpb.br>
2012-06-15 16:17:23 -0300

    Retirado import!!!

--- src/br/ufpb/iged/gui/ApresSwing.java
+++ src/br/ufpb/iged/gui/ApresSwing.java
@@ -9,13 +9,17 @@ import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import com.lowagie.text.pdf.codec.JBIG2Image;
+import br.ufpb.iged.gui.event.ClosedTelaIGEDletEvent;
+import br.ufpb.iged.gui.event.TelaIGEDletEvent;
+
+
 
 public class ApresSwing extends JFrame {
 	
@@ -91,7 +95,10 @@ public class ApresSwing extends JFrame {
 		} 	
 		
 	class WindowEventHandler extends WindowAdapter{
-		
+		public void windowClosing(WindowEvent evt) {
+				System.out.println("Window Closed!");
+            
+        }
 		
 	}
 	
