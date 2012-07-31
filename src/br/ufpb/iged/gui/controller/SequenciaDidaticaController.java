ed7b9f3c251fc45be60fa1ab535c99bf6d43688f
renanol <renan.oliveira@dce.ufpb.br>
2012-07-31 11:50:25 -0300

    novas telas

---
+++ src/br/ufpb/iged/gui/controller/SequenciaDidaticaController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufpb.iged.gui.controller;
+
+import br.ufpb.iged.gui.model.SequenciaDidatica;
+import java.util.List;
+
+/**
+ *
+ * @author darkolyver
+ */
+public class SequenciaDidaticaController {
+    
+    public List<SequenciaDidatica> listaDeSequenciaDidatica(String filtro){
+    
+        return null;        
+    
+    }
+    
+    public void salvarSequenciaDidatica(SequenciaDidatica sequenciaDidatica){
+        
+        
+    
+    }
+    
+}
---
+++ src/br/ufpb/iged/gui/model/SequenciaDidatica.java
@@ -0,0 +1,19 @@
+package br.ufpb.iged.gui.model;
+
+/**
+ *
+ * @author darkolyver
+ */
+public class SequenciaDidatica {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }    
+    
+}
